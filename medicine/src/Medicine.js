5dd16ad8cb72b29d946136d0f67586d5a2a58a73
Asfak06 <asfakmunna@gmail.com>
2021-12-21 18:09:06 +0600

    functional component using hook

--- medicine/src/Medicine.js
+++ medicine/src/Medicine.js
@@ -1,53 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
 import {HomePageHeader,MedicineData} from './components/MedicineData'
 import medicine from './components/data.json';
 
-class Medicine extends React.Component{
-      
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-            page:1
-          };  
-        this.handleChange = this.handleChange.bind(this);
-        this.paginate = this.paginate.bind(this);
-      }
+export default function Medicine(){
+    const [value, setValue] = useState(null);
+    const [page, setPage] = useState(1); 
     
-    handleChange(event) {
-        this.setState({ value : event.target.value });
+    const handleChange = (event) => {
+        setValue(event.target.value );
+    };
+
+    const paginate = (event) =>{
+         setPage(event.target.value );
+    };
+
+    var len = medicine.length/2;
+    var arr = [];
+    for(var i=1;i<=len; i++){
+        arr.push(<button key={i} onClick={paginate} value={i}>{i}</button>);
     }
 
-    paginate(event) {
-         this.setState({ page : event.target.value });
-    }
-
-    
-
-    render(){
-        var len = medicine.length/2;
-        var arr = [];
-        for(var i=1;i<=len; i++){
-            arr.push(<button key={i} onClick={this.paginate} value={i}>{i}</button>);
-        }
-
-        console.log("count")
-       return(
-          <div>
-              <HomePageHeader />
-              <select id="lang" onChange={this.handleChange}>
-                 <option value="select">Select</option>
-                 <option value="1">paracetamol</option>
-                 <option value="2">anti-biotic</option>
-              </select>
-              <p></p>
-              <MedicineData value={this.state.value}  page={this.state.page}/>
-              {arr}         
-          </div>
-       );
-    }
+    return(
+        <div>
+            <HomePageHeader />
+            <select id="lang" onChange={handleChange}>
+               <option value="select">Select</option>
+               <option value="1">paracetamol</option>
+               <option value="2">anti-biotic</option>
+            </select>
+            <p></p>
+            <MedicineData value={value}  page={page}/>
+            {arr}         
+        </div>
+     );
 };
-
-
-
-export default Medicine;
