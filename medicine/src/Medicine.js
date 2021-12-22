d93941058001b4196b3d5aa805b91c422d7ac756
Asfak06 <asfakmunna@gmail.com>
2021-12-22 14:56:03 +0600

    change table design

--- medicine/src/Medicine.js
+++ medicine/src/Medicine.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import {HomePageHeader,MedicineData} from './components/MedicineData'
 import medicine from './components/data.json';
-
+import "./components/Medicine.css";
+import "./select.scss"
 export default function Medicine(){
     const [value, setValue] = useState(null);
     const [page, setPage] = useState(1); 
@@ -17,18 +18,22 @@ export default function Medicine(){
     var len = medicine.length/2;
     var arr = [];
     for(var i=1;i<=len; i++){
-        arr.push(<button key={i} onClick={paginate} value={i}>{i}</button>);
+        arr.push(<button className='page' key={i} onClick={paginate} value={i}>{i}</button>);
     }
 
     return(
         <div>
             <HomePageHeader />
-            <select id="lang" onChange={handleChange}>
-               <option value="select">Select</option>
-               <option value="1">paracetamol</option>
-               <option value="2">anti-biotic</option>
+            <div include="form-input-select()">
+            <select required onChange={handleChange}>
+                        <option value="select">Select</option>
+                        <option value="1">paracetamol</option>
+                        <option value="2">anti-biotic</option>
             </select>
+            </div>
+
             <p></p>
+
             <MedicineData value={value}  page={page}/>
             {arr}         
         </div>
--- medicine/src/components/MedicineData.js
+++ medicine/src/components/MedicineData.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Medicine.css";
 import medicine from './data.json';
+import { FiArrowUp } from "react-icons/fi";
+import { FcApproval, FcCancel } from "react-icons/fc";
 
 export const MedicineData = ({value, page}) => {
 
@@ -18,6 +20,20 @@ export const MedicineData = ({value, page}) => {
     }
     
 
+    if(res){
+        return(            
+          <Dataset medicines={res} page={page} />
+        );
+    }
+
+    return (
+         <Dataset medicines={medicine} page={page} />
+    );
+    
+};
+
+const Dataset = ({medicines, page}) => {
+    
     var min=0;
     var max=min+1;
     if(page>1){
@@ -27,73 +43,71 @@ export const MedicineData = ({value, page}) => {
         }
     }
 
-console.log(min,max);
-
-    if(res){
-        return(
-            
-            <div className="container">
-              <table>
-                    <tbody>
-                    <tr>
-                        <td>
-                        <h3>name</h3>
-                        </td>
-                        <td>
-                        <h3>group</h3>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>                 
-            {res.map((data, key) => {
-                if(key>=min && key<=max){
-                    return (
-                        <div key={key}>
-                        <Med
-                            key={key}
-                            name={data.name}
-                            group={data.group}
-                        />
-                        </div>
-                    );
-                }
-
-            })}
-            </div>
-        );
-    }
-
+    console.log(min,max);
     return (
-        <div className="container">
-                <table>
+        <div className="container" >
+                <table id="medicines">
                     <tbody>
                     <tr>
-                        <td>
+                        <th>
+                          <p></p>
+                        </th>
+                        <th>
                         <h3>name</h3>
-                        </td>
-                        <td>
+                        </th>
+                        <th>
                         <h3>group</h3>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>            
-          {medicine.map((data, key) => {
+                        </th>
+                        <th>
+                        <h3>event</h3>
+                        </th>
+                        <th>
+                        <h3>indication</h3>
+                        </th>
+                        <th>
+                        <h3>dosageAndAdministration</h3>
+                        </th>
+                        <th>
+                        <h3>status</h3>
+                        </th>
+                        <th>
+                        <h3>image</h3>
+                        </th>
+                        <th>                        
+                        <h3><FiArrowUp /> createAt</h3>
+                        </th>
+                        <th>
+                        <h3>updatedAt</h3>
+                        </th>
+                        <th>
+                        <h3>manufacturingBy</h3>
+                        </th>                        
+                    </tr>        
+          {medicines.map((data, key) => {
             if(key>=min && key<=max){
                 return (
-                <div key={key}>                  
                     <Med
-                    key={key}
-                    name={data.name}
-                    group={data.group}
+                        key={key}
+                        name={data.name}
+                        group={data.group}
+                        event={data.event}
+                        indication={data.indication}
+                        dosageAndAdministration={data.dosageAndAdministration}
+                        preparation={data.preparation}
+                        status={data.status}
+                        image={data.image}
+                        createAt={data.createAt}
+                        updatedAt={data.updatedAt}
+                        manufacturingBy={data.manufacturingBy}
                     />
-                </div>
                 );
             }
           })}
+                </tbody>
+          </table>    
         </div> 
     );
-    
-};
+}
 
 export const HomePageHeader = () => {
     return (
@@ -103,20 +117,45 @@ export const HomePageHeader = () => {
     );
 };
 
-const Med = ({ name, group }) => {
+const Med = ({name, group, event, indication, dosageAndAdministration, status, image, createAt, updatedAt, manufacturingBy}) => {
     if (!name) return <div />;
     return (
-      <table>
-        <tbody>
           <tr>
+            <td>
+             <input className='check' type="checkbox" />
+            </td>
             <td>
               <h5>{name}</h5>
             </td>
             <td>
               <h5>{group}</h5>
             </td>
+            <td>
+              <h5>{event}</h5>
+            </td>
+            <td>
+              <h5>{indication}</h5>
+            </td>
+            <td>
+              <h5>{dosageAndAdministration}</h5>
+            </td>
+            <td>
+            {status == 'active'? <h3><FcApproval/> {status} </h3>: <h3><FcCancel/>  {status}</h3> }
+                
+
+            </td>
+            <td>
+              <img src={image} alt="Logo" />
+            </td>
+            <td>
+              <h5>{createAt}</h5>
+            </td>
+            <td>
+              <h5>{updatedAt}</h5>
+            </td>
+            <td>
+              <h5>{manufacturingBy}</h5>
+            </td>
           </tr>
-        </tbody>
-      </table>
     );
 };
